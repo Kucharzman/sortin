5770501c0c2b4190916ad130d368dd3e3c51cbc2
test <test@asd.pl>
2023-01-04 09:34:55 +0100

    1.0 - generacja randomowych liczb

--- app/src/main/java/com/example/sortin/MainActivity.kt
+++ app/src/main/java/com/example/sortin/MainActivity.kt
@@ -2,10 +2,32 @@ package com.example.sortin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import kotlin.math.sin
+import kotlin.random.Random
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val arrNums: IntArray = intArrayOf(0)
+
+        findViewById<Button>(R.id.bttRand).setOnClickListener {
+            val numAmount = findViewById<EditText>(R.id.edtAmount).text.toString().toInt()
+
+                for (i in 0..numAmount) {
+
+                    val randed = (0..100).random()
+                    arrNums[i] = randed
+
+                findViewById<TextView>(R.id.tvNumbers).text = ""
+                findViewById<TextView>(R.id.tvNumbers).text = arrNums.toString()
+            }
+        }
+
     }
 }
