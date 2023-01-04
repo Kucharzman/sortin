51e46448a7a7c68f55be621225bbc8b415c12f20
test <test@asd.pl>
2023-01-04 10:12:50 +0100

    2.0 sortowanie działa

--- app/src/main/java/com/example/sortin/MainActivity.kt
+++ app/src/main/java/com/example/sortin/MainActivity.kt
@@ -17,7 +17,7 @@ class MainActivity : AppCompatActivity() {
         var arrNums: MutableList<Int> = mutableListOf()
 
         findViewById<Button>(R.id.bttRand).setOnClickListener {
-            val numAmount = findViewById<EditText>(R.id.edtAmount).text.toString().toInt()
+            val numAmount = findViewById<EditText>(R.id.edtAmount).text.toString().toInt() - 1
                 arrNums.clear()
 
                 for (i in 0..numAmount) {
@@ -32,5 +32,46 @@ class MainActivity : AppCompatActivity() {
             }
         }
 
+        findViewById<Button>(R.id.bttSort).setOnClickListener {
+
+            //val arrSorted = quickSort(arrNums.toIntArray(),0,arrNums.size)
+            var arrNumsSorted: List<Int> = quicksort(arrNums)
+
+            findViewById<TextView>(R.id.tvNumbers).text = ""
+            for (element in arrNumsSorted) {
+                findViewById<TextView>(R.id.tvNumbers).append(element.toString()+" ")
+            }
+        }
+
     }
+
+    fun quicksort(items: List<Int>): List<Int> {
+        // Return if the input list is empty or only has 1 entry, since it's already sorted
+        if (items.size <= 1) {
+            return items
+        }
+
+        // Pick a pivot
+        val chosenItem: Int = items[items.size / 2]
+
+        // Partition items in three sets: smaller, equal and greater than chosen item
+        val smallerList: MutableList<Int> = mutableListOf()
+        val equalList: MutableList<Int> = mutableListOf()
+        val greaterList: MutableList<Int> = mutableListOf()
+        items.forEach {
+            when {
+                it < chosenItem -> smallerList.add(it)
+                it > chosenItem -> greaterList.add(it)
+                else -> equalList.add(it)
+            }
+        }
+
+        // Combine results and return
+        val sortedList: MutableList<Int> = mutableListOf()
+        sortedList.addAll(quicksort(smallerList)) // Recursive call
+        sortedList.addAll(equalList)
+        sortedList.addAll(quicksort(greaterList)) // Recursive call
+        return sortedList
+    }
+
 }
